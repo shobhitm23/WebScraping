e2059f7cf152ed0d0bd47c4e89ba74c5fe26cb62
shobhitm23 <smakhij@purdue.edu>
2018-12-25 17:45:57 +0530

    Request and utf encoding

--- scraping-images.py
+++ scraping-images.py
@@ -1,6 +1,29 @@
 import webbrowser
+from bs4 import BeautifulSoup
+import requests
+import os
+import urllib2
 
 new = 2
 query = raw_input("Enter Search Term: ")
-tabUrl = "https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
-webbrowser.open(tabUrl)
+url = "https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+
+# Opening the webpage
+#webbrowser.open(tabUrl)
+
+#soup = BeautifulSoup(urllib2.urlopen(urllib2.Request(url)), 'html.parser')
+#print(soup)
+
+headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+}
+try:
+    r = requests.get(url, headers=headers, timeout=1)
+except requests.exceptions.Timeout:
+    print("Timeout occurred")
+    
+#print(r.content)
+#print("==========================================")
+soup = BeautifulSoup(r.content, 'html.parser').encode('utf-8')
+print(soup)
+#print(soup.prettify())
